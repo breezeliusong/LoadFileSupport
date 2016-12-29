a94307d70c9c7a63f9b4441b8ff48717d97509b4
v-liuson <v-liuson@MSDNUWPSERVER1>
2016-12-29 17:29:07 +0800

    12.29

--- LoadFileSupport/MainPage.xaml.cs
+++ LoadFileSupport/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Serialization;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
